0a2437e09bd3367d956c5f979b186542ef8e4d4a
= <=>
2025-04-09 15:42:52 -0400

    Improved application of the new forms

--- gitrepo/core/views.py
+++ gitrepo/core/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from .forms import StyledUserCreationForm, StyledAuthenticationForm
 
 def home(request):
     return render(request, 'core/home.html')
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = StyledUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect(home)
+            return redirect('home')
     else:
-        form = UserCreationForm()
+        form = StyledUserCreationForm()
     return render(request, 'core/signup.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = StyledAuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('home')
     else:
-        form = AuthenticationForm()
+        form = StyledAuthenticationForm()
     return render(request, 'core/login.html', {'form': form})
 
 def logout_view(request):
